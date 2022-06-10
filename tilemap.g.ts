4e25deb5a5b92c28cbbf0a616df2d767ccdf1f95
HIMAYA2022 <107245933+HIMAYA2022@users.noreply.github.com>
2022-06-10 09:54:25 +0100

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`10001000010514050505050513050505050e0e04070d0d0d0d0d0d0d0d0d0d0d0d0d0d06070d0d0d0d0d0d0d0d0d0d0d0d0d0d06070d0d0d0d0d0d0d0d0d0d0d0d0d0d06070d0d0d0d0d0d0d0d0d0d0d0d0d0d06070d0d0d0d0d0d0d0d0d0d0d0d0d0d06070d0d0d0d0d0d0d0d0d0d0d0d0d0d06110d0d0d0d0d0d090a0d0d0d0d0d0d10070d0d0d0d0d0d0c0b0d0d0d0d0d0d060f0d0d0d0d0d0d0d0d0d0d0d0d0d0d060f0d0d0d0d0d0d0d0d0d0d0d0d0d0d060f0d0d0d0d0d0d0d0d0d0d0d0d0d0d060f0d0d0d0d0d0d0d0d0d0d0d0d0d0d060f0d0d0d0d0d0d0d0d0d0d0d0d0d0d060715150d0d0d0d0d0d0d0d0d0d0d0d0602080808080808081208080808080803`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 . . . . . . . . . . . . . . 2 
+2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+`, [myTiles.transparency16,sprites.dungeon.purpleOuterNorthWest,sprites.dungeon.purpleOuterSouthEast,sprites.dungeon.purpleOuterSouthWest,sprites.dungeon.purpleOuterNorthEast,sprites.dungeon.purpleOuterNorth0,sprites.dungeon.purpleOuterEast0,sprites.dungeon.purpleOuterWest0,sprites.dungeon.purpleOuterSouth1,sprites.dungeon.purpleInnerNorthWest,sprites.dungeon.purpleInnerNorthEast,sprites.dungeon.purpleInnerSouthEast,sprites.dungeon.purpleInnerSouthWest,sprites.dungeon.floorDark0,sprites.dungeon.purpleOuterNorth1,sprites.dungeon.purpleOuterWest1,sprites.dungeon.purpleOuterWest2,sprites.dungeon.purpleOuterEast2,sprites.dungeon.purpleOuterSouth2,sprites.dungeon.purpleOuterNorth2,sprites.dungeon.purpleSwitchUp,sprites.dungeon.stairLadder], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
